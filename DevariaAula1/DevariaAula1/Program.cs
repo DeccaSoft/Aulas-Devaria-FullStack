c5054abaea6cea574050b480a48845d888754d5a
DeccaSoft <andremazevedo@gmail.com>
2022-08-21 18:02:25 -0300

    Adicionar arquivos de projeto.

---
+++ DevariaAula1/DevariaAula1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DevariaAula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string OLA = "Olá, ";
+            string nomeCompleto;
+            int idade;
+            Console.WriteLine("Bom dia! Qual o seu Nome Completo?");
+            nomeCompleto = Console.ReadLine().ToString();
+            Console.WriteLine("E qual a sua Idade?");
+            idade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(OLA + nomeCompleto + "!");
+            Console.WriteLine("Sua Idade Atual: " + idade);
+        }
+    }
+}
